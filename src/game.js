1cdadc76accd402324d29bc3e0ad25150b25b2b9
Reinerok <reinerok@gmail.com>
2019-05-08 08:42:27 +0500

    Start

---
+++ src/game.js
@@ -0,0 +1,9 @@
+export default class Game {
+    constructor() {
+        this.score = 0;
+        this.level = 0;
+        this.lines = 0;
+        this.gameOver = false;
+        this.grid = [];
+    }
+}
